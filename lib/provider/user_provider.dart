cdfae7195c3c430e6c3cb14051c54b27ed7e23e8
Armstrong Lohãns <lohansdemelo1108@gmail.com>
2021-06-01 23:33:28 -0300

    remoção de prints

--- lib/provider/user_provider.dart
+++ lib/provider/user_provider.dart
@@ -24,8 +24,6 @@ class UserProvider {
         body: body,
       );
 
-      print(await httpResponse);
-
       return httpResponse;
     } catch (error) {
       print("createUser error - " + error.toString());
